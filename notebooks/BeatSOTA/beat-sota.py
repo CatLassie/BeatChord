b7883240a57804f8ff3cfce6d96b0d90140943e8
Rjabcsenko, Peter <e1228563@student.tuwien.ac.at>
2020-12-08 17:31:30 +0100

    - updated script

--- notebooks/BeatSOTA/beat-sota.py
+++ notebooks/BeatSOTA/beat-sota.py
@@ -47,16 +47,21 @@ print("CURRENT DEVICE:", DEVICE)
 
 # paths
 CURRENT_PATH = os.getcwd()
+MODEL_BASE_PATH = os.path.join(CURRENT_PATH, 'data/models')
+FEATURE_BASE_PATH = os.path.join(CURRENT_PATH, 'data/features')
+ANNOTATION_BASE_PATH = os.path.join(CURRENT_PATH, 'data/annotations')
+
+DATASET_NAME = '<DATASET_FOLDER_NAME>'
 
 MODEL_NAME = 'beat_sota'
-MODEL_PATH = os.path.join(CURRENT_PATH, 'models')
+MODEL_PATH = os.path.join(MODEL_BASE_PATH, DATASET_NAME)
 if not os.path.exists(MODEL_PATH):
     os.makedirs(MODEL_PATH)  
     
 FEATURE_EXT = '.feat.npy'
-FEATURE_PATH = os.path.join(CURRENT_PATH, 'features')
+FEATURE_PATH = os.path.join(FEATURE_BASE_PATH, DATASET_NAME)
 ANNOTATION_EXT = '.beats'
-ANNOTATION_PATH = os.path.join(CURRENT_PATH, 'annotations')
+ANNOTATION_PATH = os.path.join(ANNOTATION_BASE_PATH, DATASET_NAME)
 
 # feature parameters
 # SR = 44100 # samping rate
