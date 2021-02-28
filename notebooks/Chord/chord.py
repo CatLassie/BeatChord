2cda4e3499125ec02f6553bd3510115392494aba
Rjabcsenko, Peter <e1228563@student.tuwien.ac.at>
2021-02-28 23:55:32 +0100

    - updated notebook til data initialization

--- notebooks/Chord/chord.py
+++ notebooks/Chord/chord.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Beat SOTA
+# # Chord
 
 # In[ ]:
 
@@ -28,4 +28,62 @@ import scripts.chord_config as cc
 
 # feature, target, annotation initializer
 from scripts.chord_feat import init_data
+
+from scripts.chord_util import parse_annotations
+
+
+# In[ ]:
+
+
+# GLOBAL VARIABLES
+
+# random seed
+SEED = cc.SEED
+
+# cuda configuration
+USE_CUDA = cc.USE_CUDA
+DEVICE = cc.DEVICE
+print("CURRENT DEVICE:", DEVICE)
+
+# paths
+MODEL_NAME = cc.MODEL_NAME
+MODEL_PATH = cc.MODEL_PATH
+if not os.path.exists(MODEL_PATH):
+    os.makedirs(MODEL_PATH)  
+    
+FPS = cc.FPS
+
+
+# In[ ]:
+
+
+# TRAINING PARAMETERS
+
+num_epochs = cc.NUM_EPOCHS
+lr = cc.LR
+batch_size = cc.BATCH_SIZE
+patience = cc.PATIENCE
+
+
+# In[ ]:
+
+
+# COMMAND LINE SUPPORT
+
+# TODO:
+
+TRAIN = cc.TRAIN
+PREDICT = cc.PREDICT
+VERBOSE = cc.VERBOSE
+
+if VERBOSE:
+    print('\n---- EXECUTION STARTED ----\n')
+    # print('Command line arguments:\n\n', args, '\n')
+
+
+# In[ ]:
+
+
+# LOAD FEATURES AND ANNOTATIONS, COMPUTE TARGETS
+train_f, train_t, train_anno, valid_f, valid_t, valid_anno, test_f, test_t, test_anno = init_data()
 
