097822dba44d9d06b5c32cd5e5f5bc768ba637fb
Rjabcsenko, Peter <e1228563@student.tuwien.ac.at>
2021-03-10 14:34:18 +0100

    - updated chord notebook

--- notebooks/Chord/scripts/chord_config.py
+++ notebooks/Chord/scripts/chord_config.py
@@ -62,8 +62,8 @@ LR = 0.001 # reduce by a factor of five whenever <condition from paper> is reach
 # lr = 0.01 ?
 
 # context for 1 feature (e.g. 4096 frames on either side, that would be 8193)
-# FEATURE_CONTEXT = 2049 #8193 #800 #1000
-# TRAINING_HOP_SIZE = 500 #512 #40 #100
+FEATURE_CONTEXT = 5 #8193 #800 #1000
+TRAINING_HOP_SIZE = 1 #512 #40 #100
 
 BATCH_SIZE = 1
 PATIENCE = 4 #9999
