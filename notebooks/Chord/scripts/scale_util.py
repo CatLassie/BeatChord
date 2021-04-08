bb9d369d6a64d893221e69f355bab425997b0817
Rjabcsenko, Peter <e1228563@student.tuwien.ac.at>
2021-04-09 00:11:51 +0200

    - added scale util script for maj_min mapping

---
+++ notebooks/Chord/scripts/scale_util.py
@@ -0,0 +1,168 @@
+import os
+from madmom.utils import search_files
+import numpy as np
+
+
+
+scale_labels = {
+    'maj': 0,
+    'min': 1,
+    'N': 2
+}
+
+labels_to_scale = {
+    0: 'maj',
+    1: 'min',
+    2: 'N'
+}
+
+def parse_scale_annotations(anno_path_root, anno_ext, display_unique_chords_and_chord_configs = False):
+    anno_paths = search_files(anno_path_root, anno_ext)
+    annotations = [load_chords(p) for p in anno_paths]
+
+    if display_unique_chords_and_chord_configs:
+        unique_labels = set()
+        for _, anno in enumerate(annotations):
+            for _, line in enumerate(anno):
+                unique_labels.add(line[1])
+        unique_labels = list(unique_labels)
+
+        print('All unique chords:\n')
+        # [print(l, ' ==> ', root_to_target(chord_to_root(l))) for l in sorted(unique_labels)]
+        [print(l, ' ==> ', scale_to_target(chord_to_scale(l))) for l in sorted(unique_labels)]
+        print('\n')
+
+    if display_unique_chords_and_chord_configs:
+        unique_chord_configs = set()
+        for _, anno in enumerate(annotations):
+            for _, line in enumerate(anno):
+                label = line[1]
+                root = chord_to_root(label)
+                label = label.split(root)
+                unique_chord_configs.add(label[1])
+        unique_chord_configs = list(unique_chord_configs)
+        print('All unique chord configurations:\n')
+        [print(cc, ' ==>  ', scale_to_target(chord_to_scale(cc))) for cc in sorted(unique_chord_configs)]
+        print('\n')
+
+    mapped_annotations = None
+    mapped_annotations = [np.array([[line[0], scale_to_target(chord_to_scale(line[1]))] for line in anno]) for anno in annotations]
+
+    return mapped_annotations
+
+# FUNCTIONS FOR PARSING CHORD ANNOTATIONS
+
+def load_chords(path):
+    file = open(path, 'r')
+    lines = file.readlines()
+
+    time_labels = [parse_chord(l) for l in lines]
+
+    return time_labels
+
+def parse_chord(time_label_string):
+    time_label = time_label_string.split()
+
+    # fix for robbie_williams dataset (second column contains ending times for chords which are redundant)
+    if(len(time_label) == 3):
+        time_label.pop(1)
+
+    if(len(time_label) != 2):
+        raise Exception('Invalid input file format! Each line must contain exactly 1 timestamp and 1 chord!')
+
+    time_label[0] = round(float(time_label[0]), 6)
+
+    return time_label
+
+# FUNCTIONS FOR MAPPING CHORDS TO SCALE
+
+def chord_to_scale(label):
+    return 'N'
+
+def scale_to_target(scale):
+    target = scale_labels.get(scale, 'NOT_FOUND')
+
+    if(target == 'NOT_FOUND'):
+        raise Exception('Invalid scale label!')
+
+    return target
+
+# FUNCTIONS FOR MAPPING CHORDS TO ROOT
+
+def chord_to_root(label):
+    root = label[0]
+    if(len(label) > 1 and (label[1] == '#' or label[1] == 'b')):
+        root += label[1]
+    return root
+
+# FUNCTIONS FOR MAPPING CHORDS TO MAJOR/MINOR
+
+def chord_to_majmin(label):
+    if(label == 'N'):
+        return label
+
+    root = chord_to_root(label)
+    majmin = root
+
+    if(':' not in label):
+        majmin +=  ':maj'
+    else:
+        affix = label.split(':')[1]
+        affix_3 = affix[:3]
+        affix_4 = affix[:4]
+        
+        if(affix_3 == 'min' or affix_3 == 'dim' or affix_4 == 'hdim'):
+            majmin += ':min'
+        elif(affix_3 == 'maj' or affix_3 == 'aug'):
+            majmin += ':maj'
+        elif(affix_3 == 'sus'):
+            majmin = 'N'
+        else:
+            majmin += ':maj'
+
+    return majmin
+
+def majmin_to_target(majmin):
+    if(majmin == 'N'):
+        return root_to_target(majmin)
+
+    majminsplit = majmin.split(':')
+    if(len(majminsplit) != 2):
+        raise Exception('Invalid chord label format! Must be of the form "<root>:<maj/min>"')
+
+    root = majminsplit[0]
+    affix = majminsplit[1]
+    target = root_to_target(root)
+
+    if(affix == 'min'):
+        target += 13
+
+    return target
+
+# FUNCTIONS FOR MAPPING OUTPUT LABELS TO NOTES AND INTERVALS (for mir_eval evaluation)
+
+def labels_to_notataion_and_intervals(labels):
+    curr_label = labels[0]
+
+    out_labels = np.empty(0)
+    out_labels = np.append(out_labels, labels_to_scale.get(curr_label))
+
+    out_intervals = np.empty((0, 2))
+    out_intervals = np.append(out_intervals, [[0,0]], axis=0)
+
+    for i, l in enumerate(labels):
+
+        if l != curr_label:
+            time = i / 100
+            out_intervals[len(out_intervals) - 1][1] = time
+            out_intervals = np.append(out_intervals, [[time, 0]], axis=0)
+            
+            out_labels = np.append(out_labels, labels_to_scale.get(l))
+
+            curr_label = l
+
+        if i == len(labels) - 1:
+            end_time = i/100
+            out_intervals[len(out_intervals) - 1][1] = end_time
+    
+    return out_labels, out_intervals
