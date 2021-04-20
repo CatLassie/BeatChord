056040795b3dba7b6c2d222cd7413b2bb09a2362
Rjabcsenko, Peter <e1228563@student.tuwien.ac.at>
2021-04-21 00:03:04 +0200

    - updated tcn mtl config

--- notebooks/BeatChord/scripts/tcn_mtl_config.py
+++ notebooks/BeatChord/scripts/tcn_mtl_config.py
@@ -104,6 +104,9 @@ TRAIN = False
 TRAIN_EXISTING = False
 PREDICT = True
 
+TRAIN_ON_BEAT = True
+TRAIN_ON_CHORD = True
+
 ######## 2 params are exclusive, if both set to true, FRAME_ONE_START takes precedence ########
 ZERO_PAD = True # pad short videos with zeros to match context length
 FRAME_ONE_START = False # pad all videos with context/2 zeros on either side to start training from frame one
