037d7eb2eeeb1983ef7acb32582f6c64c0ec187e
Rjabcsenko, Peter <e1228563@student.tuwien.ac.at>
2021-04-26 00:09:46 +0200

    - updated notebook titles

--- notebooks/BeatChord/tcn_chord_BCE.py
+++ notebooks/BeatChord/tcn_chord_BCE.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # TCN Chord
+# # TCN Chord BCE
 
 # In[ ]:
 
--- notebooks/BeatChord/tcn_mtl_bce.py
+++ notebooks/BeatChord/tcn_mtl_bce.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # TCN MTL
+# # TCN MTL BCE
 
 # In[ ]:
 
