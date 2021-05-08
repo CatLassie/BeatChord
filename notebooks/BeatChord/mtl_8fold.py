e6ff22f11c24fd82c933f7e26e5e1a9157f70304
Rjabcsenko, Peter <e1228563@student.tuwien.ac.at>
2021-05-08 19:06:18 +0200

    - added condition for local prediction

--- notebooks/BeatChord/mtl_8fold.py
+++ notebooks/BeatChord/mtl_8fold.py
@@ -726,7 +726,10 @@ def run_prediction(test_features, fold_number):
     
     # load model
     model = TCNMTLNet().to(DEVICE)
-    model.load_state_dict(torch.load(os.path.join(MODEL_PATH, MODEL_NAME + '_split' + str(fold_number) + '.model')))
+    if DEVICE == 'CPU':
+        model.load_state_dict(torch.load(os.path.join(MODEL_PATH, MODEL_NAME + '_split' + str(fold_number) + '.model'), map_location=torch.device('cpu')))
+    else:
+        model.load_state_dict(torch.load(os.path.join(MODEL_PATH, MODEL_NAME + '_split' + str(fold_number) + '.model')))
     #print('model loaded...')
     
     # calculate actual output for the test data
